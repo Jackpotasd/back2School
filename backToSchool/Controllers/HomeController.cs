97d052a825b647c04286dafa7da6f66286853c2c
Jackpotasd <j.alatorrerangel@gmail.com>
2021-07-30 00:49:23 -0500

    Templates for register and get data

--- backToSchool/Controllers/HomeController.cs
+++ backToSchool/Controllers/HomeController.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace backToSchool.Controllers
 {
     public class HomeController : Controller
     {
-        //-------------------------------------------
-        //             Esta es la vista Index
-        //-------------------------------------------
 
         public ActionResult Index()
         {
@@ -24,27 +17,14 @@ namespace backToSchool.Controllers
         }
 
 
-
-        //-------------------------------------------
-        //             Esta es la vista About
-        //-------------------------------------------
         public ActionResult UserRegisterForm()
         {
             return View();
         }
 
 
-
-
-
-
-        //-------------------------------------------
-        //             Esta es la vista Contact
-        //-------------------------------------------
-        public ActionResult Contact()
+        public ActionResult UserGetData()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
